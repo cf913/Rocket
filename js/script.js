1694b8bd9f7dff25f139b0588a5cc809ba9c51b1
Chad FALLOTIN <chaddudz@hotmail.fr>
2017-05-05 15:01:52 +1000

    cleaned up js, adding content no longer requires updating script.js

--- js/script.js
+++ js/script.js
@@ -4,28 +4,17 @@ function toPage(e) {
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
-    
-    switch (e) {
-        case 12:
-            document.getElementById("les12").style.display = "block";
-            window.scroll(0,0);
-            break;
-        case "kickoff":
-            document.getElementById("kickoff").style.display = "block";
-            window.scroll(0,0);
-            break;
-        case "tricks":
-            document.getElementById("tricks").style.display = "block";
-            window.scroll(0,0);
-        break;
-        case "power":
-            document.getElementById("tricks").style.display = "block";
-            document.getElementById("power").scrollIntoView();
-        break;
-        case "double":
-            document.getElementById("tricks").style.display = "block";
-            document.getElementById("double").scrollIntoView();
-        break;
+    document.getElementById(e).style.display = "block";
+    window.scroll(0,0);
+}
 
+function wihinPage(page, target) {
+    var i;
+    content = document.getElementsByClassName("content");
+    for (i = 0; i < content.length; i++) {
+        content[i].style.display = "none";
     }
+    document.getElementById(page).style.display = "block";
+    document.getElementById(target).scrollIntoView();
+    
 }
