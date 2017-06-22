b21c81c646326e8885010c2bb7302f3cb5b0617c
root <root@DESKTOP-V8T7DD4.localdomain>
2017-06-22 17:32:29 +1000

    added half flip and navbar style

--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
-function toPage(e) {
-    var i;
+/**************************************** NAVBAR ********************************************/
+
+function goActive(b) {
+    $(b).parent().addClass('active').siblings().removeClass('active');
+}
+
+function goActiveGreatGrandParent(b) {
+    $(b).parent().parent().parent().addClass('active').siblings().removeClass('active');
+}
+
+function toPage(e, b, drop) {
+    var i, content;
     content = document.getElementsByClassName("content");
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
     document.getElementById(e).style.display = "block";
+    if (drop === 1) {
+        goActiveGreatGrandParent(b)
+    } else {
+        goActive(b);
+    }
     window.scroll(0,0);
 }
 
-function withinPage(page, target) {
-    var i;
+function withinPage(page, target, b) {
+    var i, content;
     content = document.getElementsByClassName("content");
     for (i = 0; i < content.length; i++) {
         content[i].style.display = "none";
     }
     document.getElementById(page).style.display = "block";
     document.getElementById(target).scrollIntoView();
+    goActiveGreatGrandParent(b);
     
 }
+
+$('ul.nav li.dropdown').hover(function () {
+  $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn(200);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeOut(0);
+});
+
+/********************************************************************************************/
+
